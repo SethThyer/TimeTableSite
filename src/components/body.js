dd9522ec8913d28f47151a8f43604bf442df08ce
SethThyer <seththyer23@gmail.com>
2021-10-06 17:56:37 +1100

    Added grid template

--- src/components/body.js
+++ src/components/body.js
@@ -1,9 +1,17 @@
+import data from '../tools/data'
 
 const Body = () => {
-    
+    console.log(data('1', 'Wednesday'));
     return (
         <body>
-            Body
+            <div className="grid">
+                <div className="grid-item">Period 1</div>
+                <div className="grid-item">Period 2</div>
+                <div className="grid-item">Period 3</div>
+                <div className="grid-item">Period 4</div>
+                <div className="grid-item">Period 5</div>
+                <div className="grid-item">Period 6</div>
+            </div>
         </body>
     );
 }
